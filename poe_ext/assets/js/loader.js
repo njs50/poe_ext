1861af48a963cf416bb646f017bed0ef62a2eb32
Nicholas J Shearer <nick@simianengineering.com>
2012-07-19 09:52:31 +1200

    added some error catching aroudn the data loader so that it's possible to refesh the local db on an error.

--- poe_ext/assets/js/loader.js
+++ poe_ext/assets/js/loader.js
@@ -366,35 +366,57 @@ function loadLeagueData(league) {
 
 		// get the first tab (and tab labels) first...		
 		getStashPage(league,0).done(function(oData){
+
+			try {
 			
-			oTabs = oData.tabs;
-			$.merge(items, responseToItems(oData, {section: 'stash', page: parseInt(oTabs[oData.tabIndex].n) }))
+				oTabs = oData.tabs;
+				$.merge(items, responseToItems(oData, {section: 'stash', page: parseInt(oTabs[oData.tabIndex].n) }))
 
-		}).done(function(){
+			} catch (e) {
 
-			for (var i=0; i < numTabs; i++ ) {
-				$('#refreshTabs').append('<li><label class="checkbox"><input type="checkbox" name="refreshTabs" value="stash-' + league + '-' + i + '">Tab:' + parseInt(oTabs[i].n) + '</label></li>');
-			}
-
-			for (var i=1; i < numTabs; i++ ) {		
-				loadQueue.addPromise(
-					getStashPage(league,i).done(function(oData){													
-						$.merge(items, responseToItems(oData, {section: 'stash', page: parseInt(oTabs[oData.tabIndex].n) }))
-					})
-				);
-			}
-
-			loadQueue.completed(function(){
-				processItems(items);
 				$.unblockUI();
-			})
-
-			loadQueue.failed(function(){		
 				$('#err').html('An error occured while requesting data from pathofexile.com. Please ' +
 							   'click refresh data to try again. If the error persists, contact the author.');
 				console.log('Error while fetching from pathofexile.com - try clicking "Refresh Data"');
+				errorDump(e);
+			}				
+
+		}).done(function(){
+
+			try {
+
+				for (var i=0; i < numTabs; i++ ) {
+					$('#refreshTabs').append('<li><label class="checkbox"><input type="checkbox" name="refreshTabs" value="stash-' + league + '-' + i + '">Tab:' + parseInt(oTabs[i].n) + '</label></li>');
+				}
+
+				for (var i=1; i < numTabs; i++ ) {		
+					loadQueue.addPromise(
+						getStashPage(league,i).done(function(oData){													
+							$.merge(items, responseToItems(oData, {section: 'stash', page: parseInt(oTabs[oData.tabIndex].n) }))
+						})
+					);
+				}
+
+				loadQueue.completed(function(){
+					processItems(items);
+					$.unblockUI();
+				})
+
+				loadQueue.failed(function(){		
+					$('#err').html('An error occured while requesting data from pathofexile.com. Please ' +
+								   'click refresh data to try again. If the error persists, contact the author.');
+					console.log('Error while fetching from pathofexile.com - try clicking "Refresh Data"');
+					$.unblockUI();
+				})
+
+			} catch (e) {
+
 				$.unblockUI();
-			})
+				$('#err').html('An error occured while requesting data from pathofexile.com. Please ' +
+							   'click refresh data to try again. If the error persists, contact the author.');
+				console.log('Error while fetching from pathofexile.com - try clicking "Refresh Data"');
+				errorDump(e);
+			}
 
 		})
 	
